cd83163a1b30301728edc230b177555fa693b1cf
oltapllana <94623315+oltapllana@users.noreply.github.com>
2024-07-03 22:11:28 +0200

    small change

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,13 @@ const { ObjectId } = require("mongodb");
 
 var app = Express();
 app.use(Express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://book-tickets.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 var CONNECTION_STRING =
   "mongodb+srv://oltapllana:Oltapllana123.,@thesis.euffiqk.mongodb.net/?retryWrites=true&w=majority&appName=thesis";
